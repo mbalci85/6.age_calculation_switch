a0aadeb101d5ec4fbb1800d7bf1193f8bd01199c
Mustafa Balci <72220498+mbalci85@users.noreply.github.com>
2020-12-18 09:03:02 +0000

    prompt and alert added

--- script.js
+++ script.js
@@ -1,34 +1,34 @@
 //John went to market and drink something. But according to rules only +18 age can buy alcohol:
 
-let ageJohn = 33;
+let ageJohn = prompt("Enter John's age: ");
 
 if (ageJohn >= 18) {
-	console.log(`John can buy beer since he is ${ageJohn} years old.`);
+	alert(`John can buy beer since he is ${ageJohn} years old.`);
 } else if (ageJohn < 18) {
-	console.log(`John can buy juice since he is ${ageJohn} years old`);
+	alert(`John can buy juice since he is ${ageJohn} years old`);
 }
 
 //ternary operator
 
 ageJohn >= 18
-	? console.log(`John can buy beer since he is ${ageJohn} years old.`)
-	: console.log(`John can buy juice since he is ${ageJohn} years old`);
+	? alert(`John can buy beer since he is ${ageJohn} years old.`)
+	: alert(`John can buy juice since he is ${ageJohn} years old`);
 
 //express John's state according to his age
 
 switch (true) {
 	case ageJohn < 13:
-		console.log(`John is a boy since he is ${ageJohn} years old`);
+		alert(`John is a boy since he is ${ageJohn} years old`);
 		break;
 	case ageJohn >= 13 && ageJohn < 20:
-		console.log(`John is a teenager since he is ${ageJohn} years old`);
+		alert(`John is a teenager since he is ${ageJohn} years old`);
 		break;
 	case ageJohn >= 20 && ageJohn < 30:
-		console.log(`John is a young man since he is ${ageJohn} years old`);
+		alert(`John is a young man since he is ${ageJohn} years old`);
 		break;
 	case ageJohn >= 30:
-		console.log(`John is a man since he is ${ageJohn} years old`);
+		alert(`John is a man since he is ${ageJohn} years old`);
 		break;
 	default:
-		console.log("John's age is not valid");
+		alert("John's age is not valid");
 }
